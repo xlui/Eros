e2d6cfc05eb45fe845283eb033493905546928de
xlui <liuqi0315@gmail.com>
2017-12-30 13:15:02 +0800

    delete exist code and restart

--- Blog/src/main/resources/data.sql
+++
@@ -1,6 +0,0 @@
-# create database eros;
-# grant all privileges on eros.* to admin@localhost identified by 'admin';
-
-insert into article VALUES
-    (1, "admin", "Hello World! This is my first blog.", "default", now(), "Hello"),
-    (2, "admin", "This is second blog. Just for test", "default", now(), "Hello");
